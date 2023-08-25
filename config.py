f2ee86d74fad4e8281e941a96f1786af713d2938
eggoisticboy <141562776+eggoisticboy@users.noreply.github.com>
2023-08-25 10:55:13 +0530

    Update config.py
    Signed-off-by: eggoisticboy <141562776+eggoisticboy@users.noreply.github.com>

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://telegra.ph/file/ba0dcc96f791475f17db0.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/bfc404a87d1912166866a.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
